8581fec42985db5195d6a5f7aebb75ada712cde9
Mike Bell <mdb036@gmail.com>
2026-06-07 18:51:46 +0100

    32 bits of output

--- src/prbs31.v
+++ src/prbs31.v
@@ -4,14 +4,14 @@ module prbs31 (
     input wire          clk,
     input wire          rst_n,
     input wire          en,
-    output reg  [30:0]  q
+    output reg  [31:0]  q
 );
 
     always @(posedge clk or negedge rst_n) begin
         if (~rst_n) q <= '1;
         else if (en) begin
             q[0] <= q[30] ^ q[27];
-            q[30:1] <= q[29:0];
+            q[31:1] <= q[30:0];
         end
     end
 
